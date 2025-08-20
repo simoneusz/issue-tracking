7558b85e53e55839426bc91b21ae190192b944d9
Ilya <kos.illia.r@gmail.com>
2025-08-20 11:46:15 +0300

    Generate default devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(_resource)
+    root_path
+  end
 end
